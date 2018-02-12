3603e2ffd52f97eeb92ac5f816a9264113a491da
itsnamgyu <itsnamgyu@gmail.com>
2018-02-12 06:21:21 +0000

    Separated common install script / moved stuff to src

--- mac_install.sh
+++ mac_install.sh
@@ -1,7 +1,2 @@
 BASE_BASH=~/.bash_profile
-echo 'BASE_BASH=$BASE_BASH' | cat - base.sh > temp && cp temp base.sh
-cat base.sh > $BASE_BASH
-mkdir ~/.bash
-cp local.sh ~/.bash
-cp alias.sh ~/.bash
-source $BASE_BASH
+source src/common_install.sh
---
+++ src/common_install.sh
@@ -0,0 +1,7 @@
+echo 'BASE_BASH=$BASE_BASH' | cat - src/base.sh > temp && cp temp src/base.sh
+cat src/base.sh > $BASE_BASH
+rm temp
+mkdir ~/.bash
+cp src/local.sh ~/.bash
+cp src/alias.sh ~/.bash
+source $BASE_BASH
