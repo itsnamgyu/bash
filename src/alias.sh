1fd72ad57d3498854b361d168aa6ed022c7e967e
Namgyu Ho <itsnamgyu@gmail.com>
2019-05-08 13:59:47 -0700

    link rba to new .bash_aliases

--- src/alias.sh
+++ src/alias.sh
@@ -1,6 +1,6 @@
 alias rv="vi ~/.vimrc"
 alias rb="vi ~/.bash_profile; source ~/.bash_profile"
-alias rba="vi ~/.bash_alias.sh; source ~/.bash_profile"
+alias rba="vi ~/.bash_aliases; source ~/.bash_profile"
 
 alias ssh-config="vi ~/.ssh/config"
 alias ssh-key="cat ~/.ssh/id_rsa.pub"
