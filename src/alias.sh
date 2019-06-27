f94bcc744e315b16d418a841d6863671cfa5a742
Namgyu Ho <itsnamgyu@gmail.com>
2019-06-26 18:46:43 -0700

    source on django alias

--- src/alias.sh
+++ src/alias.sh
@@ -47,7 +47,7 @@ alias merge="git merge"
 alias amend="git commit --amend"
 alias checkout="git checkout"
 
-alias django="python manage.py"
+alias django="source venv/bin/activate; python manage.py"
 alias startapp="django-admin startapp"
 alias runserver="source venv/bin/activate; python manage.py runserver"
 alias createsuperuser="source venv/bin/activate; python manage.py createsuperuser"
